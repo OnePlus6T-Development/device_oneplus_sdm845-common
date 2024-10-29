5ed205f8fe067d4a88c92f387e4a28da3f54c189
ShuaiB-SB233 <ShuaiB-SB233@2vzznb.onmicrosoft.com>
2024-10-29 09:14:06 +0800

    sdm845-common: Enable Android Verified Boot

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,8 +181,8 @@ SOONG_CONFIG_ONEPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(COMMON_PATH)/touch/inclu
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+#BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
