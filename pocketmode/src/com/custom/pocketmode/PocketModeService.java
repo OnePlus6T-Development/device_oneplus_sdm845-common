ef00a276c15ec1a595dba9705139c9adae35cbc5
uwu-gl <moegluwu0@gmail.com>
2025-07-06 22:02:17 +0800

    sdm845-common: pocketmode: Rebrand from Lineage
    * Some apps detect lineage strings and crash.
    
    Change-Id: Ieab0e4715e89d640c1e96c02078d03d1e642d87e
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- pocketmode/src/com/custom/pocketmode/PocketModeService.java
+++ pocketmode/src/com/custom/pocketmode/PocketModeService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.pocketmode;
+package com.custom.pocketmode;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
