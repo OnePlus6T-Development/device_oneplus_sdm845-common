5a18de6760e746634b81e9b559a8bc0caf3762dc
uwu-gl <moegluwu0@gmail.com>
2025-07-18 11:57:07 +0800

    sdm845-common: common.mk: Build libaudiohalvendorextn
    * E audioserver: Failed to load library: libaudiohalvendorextn.so (dlopen failed: library "libaudiohalvendorextn.so" not found)
    
    Change-Id: I4177e180defd5b50a29e1bde14f3df21eddd393d
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
     audio.primary.sm8050 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudiohalvendorextn \
     libbatterylistener \
     libcomprcapture \
     libexthwplugin \
