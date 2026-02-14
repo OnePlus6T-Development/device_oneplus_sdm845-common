f700ed5592b4e1b6ade7887cbcc361500659ea76
uwu-gl <moegluwu0@gmail.com>
2026-02-15 03:49:28 +0800

    sdm845-common: Set as using 4.19 kernel
    Change-Id: I7d72316292b06aacf5e17058660f8b4d9a78347d
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,6 +59,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vendor/sdm845-perf_defconfig vendor/enchilada.config
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
+TARGET_KERNEL_VERSION := 4.19
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
