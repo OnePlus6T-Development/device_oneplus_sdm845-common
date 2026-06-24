594a1a236c2e6cad2fe015031e5859baf316772f
binbindhh <bin@dhao.cc>
2026-06-24 18:36:19 +0800

    sdm845-common: Patch libwfdmmsrc_system with libgui_shim
    Change-Id: Ie7344e453a6a22ed19751daa105b0a07674377e2

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'vendor/lib/libVDBlurlessAPI_v2.so': blob_fixup()
